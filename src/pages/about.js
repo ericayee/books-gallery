325f015189b2227700636ef31882cf0c3a35a7a7
Erica Yee <23279506+ericayee@users.noreply.github.com>
2020-06-15 22:35:25 -0700

    hopefully change favicon

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ export default function About() {
     <Layout style={{ color: `teal` }}>
       <h3>About the project</h3>
       <p>This is a collection of Instagram posts I designed for a series of book recommendations on my personal account. Feel free to browse the gallery.</p>
-      <p>If you decide to check out a book, please support your local library and/or independent bookstore if possible!</p>
+      <p>If you decide to check out a book, I encourage you to support your local library and/or independent bookstore if possible!</p>
       <Footer />
     </Layout>
   )
